8f0564eebc82284a00bda1655d0a6c0f98427d63
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-07 16:43:49 +0100

    created graph class

---
+++ graph_adventure/util.py
@@ -0,0 +1,25 @@
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
+    def __init__(self):
+        self.vertices = {}
+        pass
+
+    def add_vertex(self, vertex):
+        """
+        Add a vertex to the graph.
+        """
+        self.vertices[vertex] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        # Check if v1 and v2 exists in vertices list
+        if v1 in self.vertices and v2 in self.vertices:
+            # add v2 at v1 of vertices
+            self.vertices[v1].add(v2)
+         # Otherwise
+        else:
+            # Raise an error
+            raise KeyError("That vertex does not exists")
