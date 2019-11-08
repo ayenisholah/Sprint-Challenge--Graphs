61e73629028a4d9585972fd228400cb93fb95b30
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-08 14:18:31 +0100

    created stack class

--- graph_adventure/util.py
+++ graph_adventure/util.py
@@ -23,3 +23,20 @@ class Graph:
         else:
             # Raise an error
             raise KeyError("That vertex does not exists")
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
