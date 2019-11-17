83b421cbb711efc6f291dd9485f4ffe86adb88f6
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-17 12:49:38 +0100

    defined queue class

--- graph_adventure/util.py
+++ graph_adventure/util.py
@@ -40,3 +40,20 @@ class Stack():
 
     def size(self):
         return len(self.stack)
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
